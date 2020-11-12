75e1397cf4de9b0071d81ce511e2248d64018033
vinilopes91 <vinilopes13@hotmail.com>
2020-11-11 23:48:55 -0300

    Configurado setup de testes

---
+++ src/__tests__/pages/SignIn.spec.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { render } from '@testing-library/react-native';
+import SignIn from '../../pages/SignIn';
+
+jest.mock('@react-navigation/native', () => ({
+  useNavigation: jest.fn(),
+}));
+
+describe('SignIn page', () => {
+  it('Should container email/password inputs', () => {
+    const { getByPlaceholderText } = render(<SignIn />);
+
+    expect(getByPlaceholderText('E-mail')).toBeTruthy();
+    expect(getByPlaceholderText('Senha')).toBeTruthy();
+  });
+});
